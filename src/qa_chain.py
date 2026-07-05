8c1a5048bbb7319b7caa368051b649d28f775409
Anshika Rajput <anshikarajput027@gmail.com>
2026-07-05 22:11:33 +0530

    Fix langchain retrieval chain import

--- src/qa_chain.py
+++ src/qa_chain.py
@@ -1,5 +1,5 @@
 from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains.retrieval import create_retrieval_chain
+from langchain.chains import create_retrieval_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_groq import ChatGroq
 from src.retriever import load_vectorstore
